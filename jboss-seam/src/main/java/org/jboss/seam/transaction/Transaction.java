9eef28e2e435f60b32a84dd6c541e7cfe1db9ca0
omid <omidpourhadi@gmail.com>
2015-03-08 16:54:36 +0330

    Patch JBoss AS 7 Transaction
    Patch JBoss AS 7 Transaction

--- jboss-seam/src/main/java/org/jboss/seam/transaction/Transaction.java
+++ jboss-seam/src/main/java/org/jboss/seam/transaction/Transaction.java
@@ -17,9 +17,8 @@ import org.jboss.seam.util.EJB;
 import org.jboss.seam.util.Naming;
 
 /**
- * Supports injection of a Seam UserTransaction object that
- * wraps the current JTA transaction or EJB container managed
- * transaction.
+ * Supports injection of a Seam UserTransaction object that wraps the current
+ * JTA transaction or EJB container managed transaction.
  * 
  * @author Mike Youngstrom
  * @author Gavin King
@@ -27,74 +26,80 @@ import org.jboss.seam.util.Naming;
  */
 @Name("org.jboss.seam.transaction.transaction")
 @Scope(ScopeType.EVENT)
-@Install(precedence=BUILT_IN)
+@Install(precedence = BUILT_IN)
 @BypassInterceptors
 public class Transaction
 {
-   // Event keys
-   public static final String TRANSACTION_FAILED = "org.jboss.seam.transaction.transactionFailed";
+    // Event keys
+    public static final String TRANSACTION_FAILED = "org.jboss.seam.transaction.transactionFailed";
 
-   public static UserTransaction instance()
-   {
-      return (UserTransaction) Component.getInstance(Transaction.class, ScopeType.EVENT);
-   }
-   
-   @Unwrap
-   public UserTransaction getTransaction() throws NamingException
-   {
-      try
-      {
-         return createUTTransaction();
-      }
-      catch (NameNotFoundException nnfe)
-      {
-         try
-         {
-            return createCMTTransaction();
-         }
-         catch (NameNotFoundException nnfe2)
-         {
-            return createNoTransaction();
-         }
-      }
-   }
+    public static UserTransaction instance()
+    {
+        return (UserTransaction) Component.getInstance(Transaction.class, ScopeType.EVENT);
+    }
 
-   protected UserTransaction createNoTransaction()
-   {
-      return new NoTransaction();
-   }
+    @Unwrap
+    public UserTransaction getTransaction() throws NamingException
+    {
+        try
+        {
+            return createUTTransaction();
+        }
+        catch (NameNotFoundException nnfe)
+        {
+            try
+            {
+                return createCMTTransaction();
+            }
+            catch (NameNotFoundException nnfe2)
+            {
+                return createNoTransaction();
+            }
+        }
+    }
 
-   protected UserTransaction createCMTTransaction() throws NamingException
-   {
-      return new CMTTransaction( EJB.getEJBContext() );
-   }
+    protected UserTransaction createNoTransaction()
+    {
+        return new NoTransaction();
+    }
 
-   protected UserTransaction createUTTransaction() throws NamingException
-   {
-      return new UTTransaction( getUserTransaction() );
-   }
+    protected UserTransaction createCMTTransaction() throws NamingException
+    {
+        return new CMTTransaction(EJB.getEJBContext());
+    }
 
-   protected javax.transaction.UserTransaction getUserTransaction() throws NamingException
-   {
-      InitialContext context = Naming.getInitialContext();
-      try
-      {
-         return (javax.transaction.UserTransaction) context.lookup("java:comp/UserTransaction");
-      }
-      catch (NameNotFoundException nnfe)
-      {
-         try
-         {
-            //Embedded JBoss has no java:comp/UserTransaction
-            javax.transaction.UserTransaction ut = (javax.transaction.UserTransaction) context.lookup("UserTransaction");
-            ut.getStatus(); //for glassfish, which can return an unusable UT
-            return ut;
-         }
-         catch (Exception e)
-         {
-            throw nnfe;
-         }
-      }
-   }
+    protected UserTransaction createUTTransaction() throws NamingException
+    {
+        return new UTTransaction(getUserTransaction());
+    }
+
+    protected javax.transaction.UserTransaction getUserTransaction() throws NamingException
+    {
+        InitialContext context = Naming.getInitialContext();
+        try
+        {
+            return (javax.transaction.UserTransaction) context.lookup("java:comp/UserTransaction");
+        }
+        catch (NamingException ne)
+        {
+            try
+            {
+                // Embedded JBoss has no java:comp/UserTransaction
+                javax.transaction.UserTransaction ut = (javax.transaction.UserTransaction) context.lookup("UserTransaction");
+                ut.getStatus(); // for glassfish, which can return an unusable
+                                // UT
+                return ut;
+            }
+            catch (NamingException nnfe2)
+            {
+                // Try the other JBoss location in JBoss AS7
+                return (javax.transaction.UserTransaction) context.lookup("java:jboss/UserTransaction");
+            }
+            catch (Exception e)
+            {
+                throw ne;
+            }
+        }
+    }
 
 }
